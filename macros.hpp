889cc638e9f6024bccfbe03763a8a074adb7e9c4
s1kula <s1kula.ev4@gmail.com>
2026-01-02 06:00:02 +0400

    added HTTP method marcos

---
+++ macros.hpp
@@ -0,0 +1,14 @@
+#ifndef METEOR_MACROS
+#define METEOR_MACROS
+
+#define GET 1
+#define POST 2
+#define PUT 3
+#define DELETE 4
+#define PATCH 5
+#define OPTIONS 6
+#define HEAD 7
+#define CONNECT 8
+#define TRACE 9
+
+#endif
