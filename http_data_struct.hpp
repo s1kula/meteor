0b4910072ed2e4fd40d1c143d6aa6d44367a186f
s1kula <s1kula.ev4@gmail.com>
2026-01-05 16:05:35 +0400

    added a file struct

--- http_data_struct.hpp
+++ http_data_struct.hpp
@@ -4,8 +4,14 @@
 #include <cstdint>
 #include <string>
 #include <unordered_map>
-#include <utility>
-#include <vector>
+
+struct file {
+    std::string contentDisposition; 
+    std::string name; 
+    std::string filename;
+    std::string contentType;
+    std::string binary;
+};
 
 struct httpData{
     uint16_t method;
@@ -13,6 +19,7 @@ struct httpData{
     uint16_t protocol;
     std::unordered_map<std::string, std::string> headers;
     std::unordered_map<std::string, std::string> parameters;
+    std::unordered_map<std::string, file> files;
 };
 
 #endif
